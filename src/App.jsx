d68d9146d057e97d6d4468dd232dc08c65e1c060
Rucha-Umbrajkar <umbrajkarrucha7@gmail.com>
2026-02-21 19:55:05 +0530

    added homepage

--- src/App.jsx
+++ src/App.jsx
@@ -3,29 +3,26 @@ import Sponsors from "./pages/sponsors";
 import Initiative from "./pages/initiative";
 import Team from "./pages/team";
 import Timeline from "./pages/timeline";
+import Homepage from "./pages/homepage";
 import Speakers1 from "./pages/speakers";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
-
-      {/* Common Navbar */}
       <CompassNavbar />
 
-      {/* Routing only */}
       <Routes>
+        {/* Homepage */}
+        <Route path="/" element={<Homepage />} />
 
+        {/* Other Pages */}
         <Route path="/sponsors" element={<Sponsors />} />
         <Route path="/initiative" element={<Initiative />} />
         <Route path="/teams" element={<Team />} />
         <Route path="/timeline" element={<Timeline />} />
-
         <Route path="/speakers" element={<Speakers1 />} />
-
-
       </Routes>
-
     </BrowserRouter>
   );
 }
